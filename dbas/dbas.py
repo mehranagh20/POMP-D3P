8bb5c5244c67400c588afeeb8457c02c08358d1c
mehranagh20 <mehranagh200@gmail.com>
2024-09-01 11:57:29 -0700

    Add try catch to dbas

--- dbas/dbas.py
+++ dbas/dbas.py
@@ -13,17 +13,17 @@ def run_dbas(num_iters, init_data, oracle, data_min, data_max, q=0.8, n_componen
     var = None
 
     for i in range(num_iters):
-        # try:
-        model = GaussianMixture(n_components, d, covariance_type=covariance_type, mu_init=mu, var_init=var).cuda()
-        model.fit(data, n_iter=gmm_iter)
-        data, _ = model.sample(init_data.shape[0])
-        scores = oracle(data).flatten()
-        gamma = torch.quantile(scores, q)
-        data = data[scores >= gamma, :]
-        data = torch.clamp(data, data_min, data_max)
-        # except Exception as e:
-        #     print('error in dbas at iteration', i, e)
-        #     break
+        try:
+            model = GaussianMixture(n_components, d, covariance_type=covariance_type, mu_init=mu, var_init=var).cuda()
+            model.fit(data, n_iter=gmm_iter)
+            data, _ = model.sample(init_data.shape[0])
+            scores = oracle(data).flatten()
+            gamma = torch.quantile(scores, q)
+            data = data[scores >= gamma, :]
+            data = torch.clamp(data, data_min, data_max)
+        except Exception as e:
+            print('error in dbas at iteration', i, e)
+            break
         mu = model.mu
         var = model.var
     
